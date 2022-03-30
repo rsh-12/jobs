5e170f675c0dcb46929078eb0125f846a4fb0661
rsh-12 <grinvichforum12@mail.ru>
2022-03-30 20:28:27 +0500

    Update company.sql

--- backend/jobs/sql/common/dml/query/company.sql
+++ backend/jobs/sql/common/dml/query/company.sql
@@ -11,3 +11,10 @@ SELECT bs.name, COUNT(c.id)
 FROM business_stream bs
          JOIN company c ON bs.id = c.business_stream_id
 GROUP BY bs.name;
+
+-- 3) Number of company vacancies
+SELECT bs.name, c.name, COUNT(jp.id)
+FROM business_stream bs
+         JOIN company c ON bs.id = c.business_stream_id
+         JOIN job_post jp ON c.id = jp.posted_by_id
+GROUP BY bs.name, c.name;
