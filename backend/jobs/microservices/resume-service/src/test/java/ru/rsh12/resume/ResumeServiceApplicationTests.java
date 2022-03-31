086ffcb351c36a8c5244660071cdf3483314416e
rsh-12 <grinvichforum12@mail.ru>
2022-03-31 20:11:36 +0500

    Add resume service

---
+++ backend/jobs/microservices/resume-service/src/test/java/ru/rsh12/resume/ResumeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsh12.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
