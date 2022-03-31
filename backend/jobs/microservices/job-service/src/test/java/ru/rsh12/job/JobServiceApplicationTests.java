7d0568498d41d44dac04253a5704776fa342b1e6
rsh-12 <grinvichforum12@mail.ru>
2022-03-31 20:11:22 +0500

    Add job service

---
+++ backend/jobs/microservices/job-service/src/test/java/ru/rsh12/job/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsh12.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
