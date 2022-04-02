b10b2fc67089b100ec42c1ec2f596a869b7f3a14
rsh-12 <grinvichforum12@mail.ru>
2022-04-02 06:36:33 +0500

    Add composite service

---
+++ backend/jobs/microservices/composite-service/src/test/java/ru/rsh12/composite/CompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsh12.composite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
