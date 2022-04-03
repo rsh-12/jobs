f1ef5abac833f9dea08407edb7ef330cb1ddd764
rsh-12 <grinvichforum12@mail.ru>
2022-04-03 10:56:40 +0500

    Add ComponentScan annotation

--- backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/CompanyServiceApplication.java
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/CompanyServiceApplication.java
@@ -2,7 +2,9 @@ package ru.rsh12.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("ru.rsh12") // to detect Spring Beans in the api and util projects
 @SpringBootApplication
 public class CompanyServiceApplication {
 
