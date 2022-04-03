a3f0cad7e7b1d75ed005a470f2bc5d50837b3d62
rsh-12 <grinvichforum12@mail.ru>
2022-04-03 16:16:21 +0500

    Add ComponentScan annotation

--- backend/jobs/microservices/composite-service/src/main/java/ru/rsh12/composite/CompositeServiceApplication.java
+++ backend/jobs/microservices/composite-service/src/main/java/ru/rsh12/composite/CompositeServiceApplication.java
@@ -2,7 +2,9 @@ package ru.rsh12.composite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("ru.rsh12")
 @SpringBootApplication
 public class CompositeServiceApplication {
 
