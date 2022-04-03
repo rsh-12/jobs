d10c6f80b95837ceb6dfcd8942179d62ed28b93a
rsh-12 <grinvichforum12@mail.ru>
2022-04-03 11:17:55 +0500

    Add BusinessStreamApi interface

---
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/BusinessStreamApi.java
@@ -0,0 +1,8 @@
+package ru.rsh12.api.core.company.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/branches")
+public interface BusinessStreamApi {
+
+}
