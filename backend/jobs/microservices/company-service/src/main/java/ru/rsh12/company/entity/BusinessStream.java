426cee2490a080f25c9c0894603bf962de3a3c59
rsh-12 <grinvichforum12@mail.ru>
2022-04-04 06:32:47 +0500

    Add BusinessStream entity

---
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/entity/BusinessStream.java
@@ -0,0 +1,63 @@
+package ru.rsh12.company.entity;
+/*
+ * Date: 03.04.2022
+ * Time: 4:23 PM
+ * */
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@Entity
+public class BusinessStream {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Size(max = 150, message = "Value must be less than 150 characters")
+    private String name;
+
+    public BusinessStream(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        BusinessStream that = (BusinessStream) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+}
+
