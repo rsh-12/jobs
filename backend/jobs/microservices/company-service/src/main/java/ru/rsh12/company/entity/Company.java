c8d37bdf59d43a50e24b21b02c50c744aacb183d
rsh-12 <grinvichforum12@mail.ru>
2022-04-05 08:18:59 +0500

    Add Company entity

---
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/entity/Company.java
@@ -0,0 +1,86 @@
+package ru.rsh12.company.entity;
+/*
+ * Date: 04.04.2022
+ * Time: 9:59 AM
+ * */
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    @NotBlank
+    @Size(max = 100, message = "Value must be less than 101 characters")
+    private String name;
+
+    private String description;
+
+    @PastOrPresent
+    private LocalDate establishmentDate;
+
+    @Size(max = 250, message = "Value must be less than 251 characters")
+    private String websiteUrl;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Company company = (Company) o;
+
+        if (!Objects.equals(id, company.id)) {
+            return false;
+        }
+        if (!name.equals(company.name)) {
+            return false;
+        }
+        return Objects.equals(establishmentDate, company.establishmentDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+}
