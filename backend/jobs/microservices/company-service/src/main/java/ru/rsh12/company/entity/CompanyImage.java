a709b1a2c08a3f9e4bfebbb215d0d8975e07a9ce
rsh-12 <grinvichforum12@mail.ru>
2022-04-05 08:21:04 +0500

    Add CompanyImage entity

---
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/entity/CompanyImage.java
@@ -0,0 +1,58 @@
+package ru.rsh12.company.entity;
+/*
+ * Date: 04.04.2022
+ * Time: 1:20 PM
+ * */
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@Entity
+public class CompanyImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String image;
+
+    public CompanyImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CompanyImage that = (CompanyImage) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        return image.equals(that.image);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + image.hashCode();
+        return result;
+    }
+
+}
