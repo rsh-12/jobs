ce601db46eb5ac8f82f15b6b6a4769861687660a
rsh-12 <grinvichforum12@mail.ru>
2022-04-06 06:33:13 +0500

    Add PostgreSqlTestBase abstract class

---
+++ backend/jobs/microservices/resume-service/src/test/java/ru/rsh12/resume/PostgreSqlTestBase.java
@@ -0,0 +1,27 @@
+package ru.rsh12.resume;
+/*
+ * Date: 05.04.2022
+ * Time: 8:39 AM
+ * */
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class PostgreSqlTestBase {
+
+    private static final PostgreSQLContainer<?> DB =
+            new PostgreSQLContainer<>("postgres:14-alpine");
+
+    static {
+        DB.start();
+    }
+
+    @DynamicPropertySource
+    static void dbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", DB::getJdbcUrl);
+        registry.add("spring.datasource.username", DB::getUsername);
+        registry.add("spring.datasource.password", DB::getPassword);
+    }
+
+}
