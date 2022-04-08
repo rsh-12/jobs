21bb9b10c8eca400eee89f0e394ce1b3a397656f
rsh-12 <grinvichforum12@mail.ru>
2022-04-08 07:48:20 +0500

    Extend abstract PostgreSqlTestBase class

--- backend/jobs/microservices/resume-service/src/test/java/ru/rsh12/resume/ResumeServiceApplicationTests.java
+++ backend/jobs/microservices/resume-service/src/test/java/ru/rsh12/resume/ResumeServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ResumeServiceApplicationTests {
+class ResumeServiceApplicationTests extends PostgreSqlTestBase {
 
     @Test
     void contextLoads() {
