bb562736efd193bf94d79f8eae4b8d52c13e0218
rsh-12 <grinvichforum12@mail.ru>
2022-04-08 10:02:56 +0500

    Add init sql script

---
+++ backend/jobs/microservices/company-service/src/test/resources/ru/rsh12/company/repository/industries.sql
@@ -0,0 +1,11 @@
+INSERT INTO business_stream(name)
+VALUES ('Transportation, Logistics, Warehousing, International Logistics'),
+       ('IT, System Integration, Internet'),
+       ('Electronics, Tool Engineering, Household Appliances, Computers and Office Equipment'),
+       ('Telecommunications, Communications'),
+       ('Media, Marketing, Advertising, PR, Design, Production'),
+       ('Construction, Real Estate, Architecture'),
+       ('Automotive Business'),
+       ('Timber Industry'),
+       ('Metallurgy, Metalwork'),
+       ('Food Products');
