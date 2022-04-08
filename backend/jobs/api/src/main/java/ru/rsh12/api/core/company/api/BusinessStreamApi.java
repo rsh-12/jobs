77024c9ba893a941c03cb02e1196c0422d928c50
rsh-12 <grinvichforum12@mail.ru>
2022-04-08 08:17:57 +0500

    Update BusinessStreamApi

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/BusinessStreamApi.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/BusinessStreamApi.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.rsh12.api.core.company.dto.BusinessStreamDto;
 
-@RequestMapping("/api/v1/branches")
+@RequestMapping("/api/v1/industries")
 public interface BusinessStreamApi {
 
     @GetMapping(value = "/{businessStreamId}", produces = "application/json")
