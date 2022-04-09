2619024c817fbe5174ced58ca293447727682e49
rsh-12 <grinvichforum12@mail.ru>
2022-04-09 22:38:54 +0500

    Add some queries

--- backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/repository/CompanyRepository.java
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/repository/CompanyRepository.java
@@ -1,9 +1,15 @@
 package ru.rsh12.company.repository;
 
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.rsh12.company.entity.Company;
 
-public interface CompanyRepository extends
-        PagingAndSortingRepository<Company, Integer> {
+public interface CompanyRepository extends PagingAndSortingRepository<Company, Integer> {
+
+    Optional<Company> findByName(String name);
+
+    Page<Company> findByNameContainsIgnoreCase(String name, Pageable pageable);
 
 }
