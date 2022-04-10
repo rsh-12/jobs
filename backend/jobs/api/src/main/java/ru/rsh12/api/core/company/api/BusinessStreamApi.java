57fc4a6ca80e2749d1f356f63a988502faed8658
rsh-12 <grinvichforum12@mail.ru>
2022-04-10 19:54:30 +0500

    Update BusinessStreamApi

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/BusinessStreamApi.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/BusinessStreamApi.java
@@ -3,12 +3,20 @@ package ru.rsh12.api.core.company.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.company.dto.BusinessStreamDto;
 
 @RequestMapping("/api/v1/industries")
 public interface BusinessStreamApi {
 
-    @GetMapping(value = "/{businessStreamId}", produces = "application/json")
-    BusinessStreamDto getBusinessStream(@PathVariable Integer businessStreamId);
+    @GetMapping(value = "/{id}", produces = "application/json")
+    Mono<BusinessStreamDto> getBusinessStream(@PathVariable Integer id);
+
+    @GetMapping(produces = "application/json")
+    Flux<BusinessStreamDto> getBusinessStreams(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size);
 
 }
