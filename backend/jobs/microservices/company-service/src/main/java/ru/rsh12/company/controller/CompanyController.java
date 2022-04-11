e8197a2b2e13d4917803deea3fc81dd05bf458b8
rsh-12 <grinvichforum12@mail.ru>
2022-04-11 21:06:30 +0500

    Update CompanyController

--- backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/controller/CompanyController.java
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/controller/CompanyController.java
@@ -4,26 +4,36 @@ package ru.rsh12.company.controller;
  * Time: 10:42 AM
  * */
 
-import java.time.Instant;
-import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.company.api.CompanyApi;
 import ru.rsh12.api.core.company.dto.CompanyDto;
+import ru.rsh12.company.service.CompanyService;
+import ru.rsh12.company.service.mapper.CompanyMapper;
 
 @RestController
+@RequiredArgsConstructor
 public class CompanyController implements CompanyApi {
 
-    @Override
-    public CompanyDto getCompany(Integer companyId) {
+    private final CompanyService service;
+    private final CompanyMapper mapper;
 
-        return new CompanyDto(1,
-                "IT Cat",
-                "B2B services",
-                LocalDate.now().minusYears(5),
-                "",
-                2,
-                Instant.now(),
-                Instant.now());
+    @Override
+    public Mono<CompanyDto> getCompany(Integer companyId) {
+        return service.findOne(companyId)
+                .map(mapper::entityToDto);
+    }
+
+    @Override
+    public Flux<CompanyDto> getCompanies(int page, int size) {
+        page = Math.max(page, 0);
+        size = Math.max(size, 1);
+
+        return service.findAll(PageRequest.of(page, size))
+                .map(mapper::entityToDto);
     }
 
 }
