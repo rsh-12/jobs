c2ebabd7e41a57984992a2df581a87dbe69e3223
rsh-12 <grinvichforum12@mail.ru>
2022-04-12 22:48:46 +0500

    Add findByCompany query

--- backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/repository/CompanyImageRepository.java
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/repository/CompanyImageRepository.java
@@ -1,9 +1,13 @@
 package ru.rsh12.company.repository;
 
+import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import ru.rsh12.company.entity.Company;
 import ru.rsh12.company.entity.CompanyImage;
 
 public interface CompanyImageRepository extends
         PagingAndSortingRepository<CompanyImage, Integer> {
+
+    List<CompanyImage> findByCompany(Company company);
 
 }
