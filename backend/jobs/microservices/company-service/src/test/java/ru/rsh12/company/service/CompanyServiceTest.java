b3493438220e3f73ccae1a365929851b88bd500f
rsh-12 <grinvichforum12@mail.ru>
2022-04-12 16:44:44 +0500

    Add CompanyServiceTest class

---
+++ backend/jobs/microservices/company-service/src/test/java/ru/rsh12/company/service/CompanyServiceTest.java
@@ -0,0 +1,70 @@
+package ru.rsh12.company.service;
+/*
+ * Date: 12.04.2022
+ * Time: 4:33 PM
+ * */
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+import ru.rsh12.company.entity.Company;
+import ru.rsh12.company.repository.CompanyRepository;
+import ru.rsh12.company.service.impl.CompanyServiceImpl;
+
+@ExtendWith(SpringExtension.class)
+public class CompanyServiceTest {
+
+    @InjectMocks
+    private CompanyServiceImpl service;
+
+    @Mock
+    private CompanyRepository repository;
+
+    @Mock
+    private Scheduler jdbcScheduler;
+
+    @Test
+    public void findAll_ShouldReturnEmpty() {
+        given(repository.findAll(any(Pageable.class)))
+                .willReturn(Page.empty());
+
+        given(jdbcScheduler.createWorker())
+                .willReturn(Schedulers.single().createWorker());
+
+        Flux<Company> flux = service.findAll(PageRequest.of(0, 10));
+        StepVerifier.create(flux).verifyComplete();
+    }
+
+    @Test
+    public void findAll_ShouldReturnData() {
+        Company mockEntity = mock(Company.class);
+        given(mockEntity.getName()).willReturn("Sample");
+
+        given(repository.findAll(any(Pageable.class)))
+                .willReturn(new PageImpl<>(List.of(mockEntity)));
+
+        given(jdbcScheduler.createWorker())
+                .willReturn(Schedulers.single().createWorker());
+
+        Flux<Company> flux = service.findAll(PageRequest.of(0, 10));
+        StepVerifier.create(flux)
+                .thenConsumeWhile(company -> company.getName().equals("Sample"))
+                .verifyComplete();
+    }
+
+}
