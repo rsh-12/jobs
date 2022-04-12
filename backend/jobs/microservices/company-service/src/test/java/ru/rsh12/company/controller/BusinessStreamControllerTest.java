2c4ce847ad9f6a8b84e164be9e1dc2297a056f16
rsh-12 <grinvichforum12@mail.ru>
2022-04-12 18:26:48 +0500

    Update BusinessStreamControllerTest class

--- backend/jobs/microservices/company-service/src/test/java/ru/rsh12/company/controller/BusinessStreamControllerTest.java
+++ backend/jobs/microservices/company-service/src/test/java/ru/rsh12/company/controller/BusinessStreamControllerTest.java
@@ -4,22 +4,27 @@ package ru.rsh12.company.controller;
  * Time: 12:10 PM
  * */
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import lombok.NonNull;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient.BodyContentSpec;
+import reactor.core.publisher.Mono;
+import ru.rsh12.api.core.company.dto.BusinessStreamDto;
 import ru.rsh12.company.PostgreSqlTestBase;
 import ru.rsh12.company.entity.BusinessStream;
-import ru.rsh12.company.repository.BusinessStreamRepository;
+import ru.rsh12.company.service.BusinessStreamService;
+import ru.rsh12.company.service.mapper.BusinessStreamMapper;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BusinessStreamControllerTest extends PostgreSqlTestBase {
@@ -27,33 +32,25 @@ public class BusinessStreamControllerTest extends PostgreSqlTestBase {
     @Autowired
     private WebTestClient client;
 
-    @Autowired
-    private BusinessStreamRepository repository;
+    @MockBean
+    private BusinessStreamService service;
+
+    @MockBean
+    private BusinessStreamMapper mapper;
 
     private final String API = "/api/v1/industries";
 
-    private BusinessStream savedEntity;
-
-    @BeforeEach
-    void setUp() {
-        repository.deleteAll();
-        assertEquals(0, repository.count());
-
-        savedEntity = repository.save(new BusinessStream("Food Products"));
-        assertTrue(repository.count() > 0);
-    }
-
     @Test
-    public void getBusinessStreamById() {
-        getAndVerify("/" + savedEntity.getId(), HttpStatus.OK)
-                .jsonPath("$.id").isEqualTo(savedEntity.getId())
-                .jsonPath("$.name").isEqualTo(savedEntity.getName());
-    }
+    public void getBusinessStream() {
+        BusinessStream mockEntity = mock(BusinessStream.class);
+        given(service.findOne(anyInt())).willReturn(Mono.just(mockEntity));
 
-    @Test
-    public void getBusinessStreams() {
-        getAndVerify(HttpStatus.OK)
-                .jsonPath("$.length()").isEqualTo(1);
+        BusinessStreamDto dto = new BusinessStreamDto(1, "Food Products");
+        given(mapper.entityToDto(any())).willReturn(dto);
+
+        getAndVerify("/1", HttpStatus.OK)
+                .jsonPath("id").isEqualTo(1)
+                .jsonPath("name").isEqualTo("Food Products");
     }
 
     private BodyContentSpec getAndVerify(String arg, HttpStatus status) {
