c80ec5d80e1044da9969b4c748da562976e15991
rsh-12 <grinvichforum12@mail.ru>
2022-04-12 15:10:26 +0500

    Update CompanyMapper

--- backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/service/mapper/CompanyMapper.java
+++ backend/jobs/microservices/company-service/src/main/java/ru/rsh12/company/service/mapper/CompanyMapper.java
@@ -4,21 +4,32 @@ package ru.rsh12.company.service.mapper;
  * Time: 11:39 PM
  * */
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.rsh12.api.core.company.dto.CompanyDto;
 import ru.rsh12.company.entity.Company;
+import ru.rsh12.company.entity.CompanyImage;
 
 @Component
+@RequiredArgsConstructor
 public class CompanyMapper {
 
+    private final BusinessStreamMapper mapper;
+
     public CompanyDto entityToDto(Company entity) {
+        List<String> images = entity.getImages().stream()
+                .map(CompanyImage::getImage)
+                .toList();
+
         return new CompanyDto(
                 entity.getId(),
                 entity.getName(),
                 entity.getDescription(),
                 entity.getEstablishmentDate(),
                 entity.getWebsiteUrl(),
-                entity.getBusinessStream().getId(),
+                mapper.entityToDto(entity.getBusinessStream()),
+                images,
                 entity.getCreatedAt(),
                 entity.getUpdatedAt()
         );
