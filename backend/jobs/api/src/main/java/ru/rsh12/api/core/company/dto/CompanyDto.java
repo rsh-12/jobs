5f72a75496b90cb01353d7a1e30f85f302fb2801
rsh-12 <grinvichforum12@mail.ru>
2022-04-12 15:10:14 +0500

    Update CompanyDto

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/company/dto/CompanyDto.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/company/dto/CompanyDto.java
@@ -8,15 +8,19 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.List;
 
+// todo: add some validation
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
-public record CompanyDto(Integer id,
-                         String name,
-                         String description,
-                         LocalDate establishmentDate,
-                         String websiteUrl,
-                         Integer businessStreamId,
-                         Instant createdAt,
-                         Instant updatedAt) {
+public record CompanyDto(
+        Integer id,
+        String name,
+        String description,
+        LocalDate establishmentDate,
+        String websiteUrl,
+        BusinessStreamDto businessStreamDto,
+        List<String> images,
+        Instant createdAt,
+        Instant updatedAt) {
 
 }
