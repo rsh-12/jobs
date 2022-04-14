7617673fb91f660003aec6e5b54aa91eb5585236
rsh-12 <grinvichforum12@mail.ru>
2022-04-14 05:51:59 +0500

    Add JobType entity

---
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/entity/JobType.java
@@ -0,0 +1,64 @@
+package ru.rsh12.job.entity;
+/*
+ * Date: 12.04.2022
+ * Time: 10:56 PM
+ * */
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
+public class JobType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Max(value = 100, message = "Value must be less than 101 characters")
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public JobType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        JobType jobType = (JobType) o;
+
+        if (!Objects.equals(id, jobType.id)) {
+            return false;
+        }
+        return name.equals(jobType.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+}
