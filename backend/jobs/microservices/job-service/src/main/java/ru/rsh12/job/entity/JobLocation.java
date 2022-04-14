0602f84434400931f44c9ef2e6c9d1c18c03a834
rsh-12 <grinvichforum12@mail.ru>
2022-04-14 05:55:56 +0500

    Add JobLocation entity

---
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/entity/JobLocation.java
@@ -0,0 +1,89 @@
+package ru.rsh12.job.entity;
+/*
+ * Date: 13.04.2022
+ * Time: 6:26 AM
+ * */
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
+public class JobLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Max(100)
+    @NotBlank
+    private String street;
+
+    @Max(100)
+    @NotBlank
+    private String city;
+
+    @Max(100)
+    @NotBlank
+    private String state;
+
+    @Max(100)
+    @NotBlank
+    private String country;
+
+    public JobLocation(String street, String city, String state, String country) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        JobLocation that = (JobLocation) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        if (!Objects.equals(street, that.street)) {
+            return false;
+        }
+        if (!Objects.equals(city, that.city)) {
+            return false;
+        }
+        if (!Objects.equals(state, that.state)) {
+            return false;
+        }
+        return Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (street != null ? street.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        return result;
+    }
+
+}
