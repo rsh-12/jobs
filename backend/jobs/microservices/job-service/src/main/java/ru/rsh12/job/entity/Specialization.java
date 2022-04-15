9c0e516de7ef9f1c2021445e6001d2d6fdf941ff
rsh-12 <grinvichforum12@mail.ru>
2022-04-15 07:33:58 +0500

    Add Specialization entity

---
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/entity/Specialization.java
@@ -0,0 +1,71 @@
+package ru.rsh12.job.entity;
+/*
+ * Date: 13.04.2022
+ * Time: 7:09 AM
+ * */
+
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
+public class Specialization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Max(70)
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<JobPost> jobPosts;
+
+    public Specialization(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Specialization that = (Specialization) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+
+}
