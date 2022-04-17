4faa809f4b98d3b24bcbc8caaf2da0785ef4a91e
rsh-12 <grinvichforum12@mail.ru>
2022-04-17 18:35:28 +0500

    Add native query

--- backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/repository/JobPostRepository.java
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/repository/JobPostRepository.java
@@ -4,9 +4,21 @@ package ru.rsh12.job.repository;
  * Time: 7:13 PM
  * */
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import ru.rsh12.job.entity.JobPost;
 
 public interface JobPostRepository extends PagingAndSortingRepository<JobPost, Integer> {
+
+    @Query(
+            nativeQuery = true,
+            value = """
+                    SELECT DISTINCT jp.* FROM job_post jp
+                    JOIN specialization_job_post sjb ON sjb.job_post_id = jp.id
+                    WHERE sjb.specialization_id IN (:ids);
+                    """)
+    List<JobPost> findBySpecializationsIds(@Param("ids") Iterable<Integer> specializationIds);
 
 }
