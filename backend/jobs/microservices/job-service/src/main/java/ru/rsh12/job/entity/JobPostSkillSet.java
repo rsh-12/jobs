99a32aa2a72821970858d4562a00c0ca68def032
rsh-12 <grinvichforum12@mail.ru>
2022-04-17 08:27:45 +0500

    Add JobPostSkillSet entity

---
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/entity/JobPostSkillSet.java
@@ -0,0 +1,85 @@
+package ru.rsh12.job.entity;
+/*
+ * Date: 13.04.2022
+ * Time: 7:03 AM
+ * */
+
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity(name = "job_post_skill_set")
+@ToString
+@NoArgsConstructor
+public class JobPostSkillSet {
+
+    @EmbeddedId
+    private JobPostSkillSetId id;
+
+    @Max(50)
+    @NotBlank
+    @Column(nullable = false)
+    private String level;
+
+    @NotNull
+    @Column(name = "skill_set_id", insertable = false, updatable = false)
+    private Integer skillSetId;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "job_post_id", insertable=false, updatable=false)
+    private JobPost jobPost;
+
+    public JobPostSkillSet(String level, Integer skillSetId, JobPost jobPost) {
+        this.level = level;
+        this.skillSetId = skillSetId;
+        this.jobPost = jobPost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        JobPostSkillSet that = (JobPostSkillSet) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        if (!level.equals(that.level)) {
+            return false;
+        }
+        if (!skillSetId.equals(that.skillSetId)) {
+            return false;
+        }
+        return jobPost.equals(that.jobPost);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + level.hashCode();
+        result = 31 * result + skillSetId.hashCode();
+        result = 31 * result + jobPost.hashCode();
+        return result;
+    }
+
+}
