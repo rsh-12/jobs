7e12c4e28f9612e452fd69a92d0937fd4dd694ff
rsh-12 <grinvichforum12@mail.ru>
2022-04-18 08:13:39 +0500

    Add findByNameIgnoreCase query

--- backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/repository/JobTypeRepository.java
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/repository/JobTypeRepository.java
@@ -1,8 +1,11 @@
 package ru.rsh12.job.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import ru.rsh12.job.entity.JobType;
 
 public interface JobTypeRepository extends CrudRepository<JobType, Integer> {
+
+    Optional<JobType> findByNameIgnoreCase(String name);
 
 }
