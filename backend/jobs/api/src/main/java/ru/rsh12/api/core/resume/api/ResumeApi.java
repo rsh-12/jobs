f19e5735df446f3bc879a32d79c2bf9a7ad5c2b0
rsh-12 <grinvichforum12@mail.ru>
2022-04-19 09:27:17 +0500

    Update ResumeApi

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/resume/api/ResumeApi.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/resume/api/ResumeApi.java
@@ -3,12 +3,20 @@ package ru.rsh12.api.core.resume.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.resume.dto.ResumeDto;
 
-@RequestMapping("/api/v1/resume")
+@RequestMapping("/api/v1/resumes")
 public interface ResumeApi {
 
     @GetMapping(value = "/{resumeId}", produces = "application/json")
-    ResumeDto getResume(@PathVariable Integer resumeId);
+    Mono<ResumeDto> getResume(@PathVariable Integer resumeId);
+
+    @GetMapping(produces = "application/json")
+    Flux<ResumeDto> getResumes(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size);
 
 }
