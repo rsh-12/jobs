a42e52237b0022174d5a0860f6ce475d731427ae
rsh-12 <grinvichforum12@mail.ru>
2022-04-19 08:54:18 +0500

    Add JobTypeMapper

---
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/service/mapper/JobTypeMapper.java
@@ -0,0 +1,25 @@
+package ru.rsh12.job.service.mapper;
+/*
+ * Date: 18.04.2022
+ * Time: 9:46 AM
+ * */
+
+import org.springframework.stereotype.Component;
+import ru.rsh12.api.core.job.dto.JobTypeDto;
+import ru.rsh12.job.entity.JobType;
+import ru.rsh12.util.Mapper;
+
+@Component
+public class JobTypeMapper implements Mapper<JobType, JobTypeDto> {
+
+    @Override
+    public JobType dtoToEntity(JobTypeDto dto) {
+        return new JobType(dto.name());
+    }
+
+    @Override
+    public JobTypeDto entityToDto(JobType entity) {
+        return new JobTypeDto(entity.getId(), entity.getName());
+    }
+
+}
