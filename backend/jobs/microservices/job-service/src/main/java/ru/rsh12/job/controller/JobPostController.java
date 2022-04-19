b4c75c099e64651de46953d9401142ae280ac962
rsh-12 <grinvichforum12@mail.ru>
2022-04-19 08:50:10 +0500

    Update JobPostController

--- backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/controller/JobPostController.java
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/controller/JobPostController.java
@@ -5,24 +5,35 @@ package ru.rsh12.job.controller;
  * */
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.job.api.JobPostApi;
 import ru.rsh12.api.core.job.dto.JobPostDto;
+import ru.rsh12.job.service.JobPostService;
+import ru.rsh12.job.service.mapper.JobPostMapper;
 
 @RestController
 @RequiredArgsConstructor
 public class JobPostController implements JobPostApi {
 
+    private final JobPostService service;
+    private final JobPostMapper mapper;
+
     @Override
     public Mono<JobPostDto> getJobPost(Integer jobPostId) {
-        return null;
+        return service.findOne(jobPostId)
+                .map(mapper::entityToDto);
     }
 
     @Override
     public Flux<JobPostDto> getJobPosts(int page, int size) {
-        return null;
+        page = Math.max(page, 0);
+        size = Math.max(size, 1);
+
+        return service.findAll(PageRequest.of(page, size))
+                .map(mapper::entityToDto);
     }
 
 }
