9fb69ab599376b472bb025e8eeb644b32621f449
rsh-12 <grinvichforum12@mail.ru>
2022-04-19 08:54:10 +0500

    Add JobPostSkillSetDto

---
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/job/dto/JobPostSkillSetDto.java
@@ -0,0 +1,12 @@
+package ru.rsh12.api.core.job.dto;
+/*
+ * Date: 18.04.2022
+ * Time: 10:12 AM
+ * */
+
+public record JobPostSkillSetDto(
+        String level,
+        Integer skillSetId,
+        Integer jobPostId) {
+
+}
