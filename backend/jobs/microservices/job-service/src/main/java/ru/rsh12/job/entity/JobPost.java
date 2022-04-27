4d4adabe88441028a387543481aa935bf91e74bd
Rakhkmonov <get_info@bk.ru>
2022-04-27 21:45:54 +0500

    Remove JoinColumn annotation

--- backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/entity/JobPost.java
+++ backend/jobs/microservices/job-service/src/main/java/ru/rsh12/job/entity/JobPost.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Pattern.Flag;
 import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -89,7 +90,6 @@ public class JobPost {
 
     @OneToMany
     @ToString.Exclude
-    @JoinColumn(name = "job_post_id")
     private Set<JobPostSkillSet> skills = new HashSet<>();
 
     @OneToMany
