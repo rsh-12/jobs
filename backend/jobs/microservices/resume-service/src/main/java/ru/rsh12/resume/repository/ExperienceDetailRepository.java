f082a5831baae1df7539a111dd9c3fc317dcb339
Rakhmonov <get_info@bk.ru>
2022-05-02 19:37:27 +0500

    Add ExperienceDetailRepository

---
+++ backend/jobs/microservices/resume-service/src/main/java/ru/rsh12/resume/repository/ExperienceDetailRepository.java
@@ -0,0 +1,8 @@
+package ru.rsh12.resume.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.rsh12.resume.entity.ExperienceDetail;
+
+public interface ExperienceDetailRepository extends CrudRepository<ExperienceDetail, Integer> {
+
+}
