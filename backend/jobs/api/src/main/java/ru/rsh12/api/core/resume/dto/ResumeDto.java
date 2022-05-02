1162e5af94cc7526cb0124fb74248ee77430e87b
Rakhmonov <get_info@bk.ru>
2022-05-02 19:34:48 +0500

    Update ResumeDto

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/resume/dto/ResumeDto.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/resume/dto/ResumeDto.java
@@ -5,14 +5,23 @@ package ru.rsh12.api.core.resume.dto;
  * */
 
 import java.time.Instant;
+import java.util.Set;
 
-public record ResumeDto(Integer id,
-                        String description,
-                        Integer salary,
-                        String currency,
-                        String desiredJobPosition,
-                        String accountId,
-                        Instant createdAt,
-                        Instant updatedAt) {
+public record ResumeDto(
+        Integer id,
+        String description,
+        Integer salary,
+        String currency,
+        String desiredJobPosition,
+        String accountId,
+        Instant createdAt,
+        Instant updatedAt,
+        Set<CountryDto> citizenship,
+        Set<SkillSetDto> skills,
+        Set<LanguageDto> languages,
+        Set<EducationDetailDto> education,
+        Set<ExperienceDetailDto> experience,
+        Set<Integer> specializationIds
+) {
 
 }
