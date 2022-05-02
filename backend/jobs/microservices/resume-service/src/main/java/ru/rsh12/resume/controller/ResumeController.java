ba25a05a72a1726d9a916aaf6a34e1e9facad4c9
Rakhmonov <get_info@bk.ru>
2022-05-02 19:34:59 +0500

    Implement ResumeController

--- backend/jobs/microservices/resume-service/src/main/java/ru/rsh12/resume/controller/ResumeController.java
+++ backend/jobs/microservices/resume-service/src/main/java/ru/rsh12/resume/controller/ResumeController.java
@@ -4,23 +4,35 @@ package ru.rsh12.resume.controller;
  * Time: 11:28 AM
  * */
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.resume.api.ResumeApi;
 import ru.rsh12.api.core.resume.dto.ResumeDto;
+import ru.rsh12.resume.service.ResumeService;
+import ru.rsh12.resume.service.mapper.ResumeMapper;
 
 @RestController
+@RequiredArgsConstructor
 public class ResumeController implements ResumeApi {
+
+    private final ResumeService service;
+    private final ResumeMapper mapper;
 
     @Override
     public Mono<ResumeDto> getResume(Integer resumeId) {
-        return null;
+        return service.findOne(resumeId).map(mapper::entityToDto);
     }
 
     @Override
     public Flux<ResumeDto> getResumes(int page, int size) {
-        return null;
+        page = Math.max(page, 0);
+        size = Math.max(size, 1);
+
+        return service.findAll(PageRequest.of(page, size))
+                .map(mapper::entityToDto);
     }
 
 }
