e2d79ae528718dba420c8527beeae9838e96d06b
Rakhmonov <get_info@bk.ru>
2022-05-03 21:54:20 +0500

    Remove RequestMapping annotation

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/CompanyApi.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/company/api/CompanyApi.java
@@ -6,19 +6,17 @@ package ru.rsh12.api.core.company.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.company.dto.CompanyDto;
 
-@RequestMapping("/api/v1/companies")
 public interface CompanyApi {
 
-    @GetMapping(value = "/{companyId}", produces = "application/json")
+    @GetMapping(value = "/api/v1/companies/{companyId}", produces = "application/json")
     Mono<CompanyDto> getCompany(@PathVariable Integer companyId);
 
-    @GetMapping(produces = "application/json")
+    @GetMapping(value = "/api/v1/companies", produces = "application/json")
     Flux<CompanyDto> getCompanies(
             @RequestParam(required = false, defaultValue = "0") int page,
             @RequestParam(required = false, defaultValue = "10") int size);
--- backend/jobs/api/src/main/java/ru/rsh12/api/core/job/api/JobPostApi.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/job/api/JobPostApi.java
@@ -2,19 +2,17 @@ package ru.rsh12.api.core.job.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.rsh12.api.core.job.dto.JobPostDto;
 
-@RequestMapping("/api/v1/jobs")
 public interface JobPostApi {
 
-    @GetMapping(value = "/{jobPostId}", produces = "application/json")
+    @GetMapping(value = "/api/v1/jobs/{jobPostId}", produces = "application/json")
     Mono<JobPostDto> getJobPost(@PathVariable Integer jobPostId);
 
-    @GetMapping(produces = "application/json")
+    @GetMapping(value = "/api/v1/jobs", produces = "application/json")
     Flux<JobPostDto> getJobPosts(
             @RequestParam(required = false, defaultValue = "0") int page,
             @RequestParam(required = false, defaultValue = "10") int size);
