0409ca17c85500baa3fccaf1f488d4f564de3827
Rakhmonov <get_info@bk.ru>
2022-05-04 20:54:37 +0500

    Update CompositeIntegration

--- backend/jobs/microservices/composite-service/src/main/java/ru/rsh12/composite/controller/CompositeIntegration.java
+++ backend/jobs/microservices/composite-service/src/main/java/ru/rsh12/composite/controller/CompositeIntegration.java
@@ -23,42 +23,77 @@ public class CompositeIntegration implements CompanyApi, ResumeApi, JobPostApi {
 
     private final WebClient webClient;
 
+    private final String companyServiceUrl = "http://localhost:7001/api/v1/companies";
+    private final String resumeServiceUrl = "http://localhost:7002/api/v1/resumes";
+    private final String jobServiceUrl = "http://localhost:7003/api/v1/jobs";
+
     public CompositeIntegration(WebClient.Builder webClient) {
         this.webClient = webClient.build();
     }
 
+    // todo replace InvalidInputException
     @Override
     public Mono<CompanyDto> getCompany(Integer companyId) {
-        return webClient.get().uri("http://localhost:7001/api/v1/companies/" + companyId)
+        return webClient.get().uri(companyServiceUrl + "/" + companyId)
                 .retrieve()
                 .bodyToMono(CompanyDto.class)
                 .log(log.getName(), Level.FINE)
-                .onErrorMap(throwable -> new InvalidInputException("Something went wrong, oh no!"));
+                .onErrorMap(throwable -> new InvalidInputException("Temporary placeholder :)"));
     }
 
+    // todo replace InvalidInputException
     @Override
     public Flux<CompanyDto> getCompanies(int page, int size) {
-        return null;
+        return webClient.get().uri(companyServiceUrl, uriBuilder -> uriBuilder
+                        .queryParam("page", page)
+                        .queryParam("size", size)
+                        .build())
+                .retrieve()
+                .bodyToFlux(CompanyDto.class)
+                .log(log.getName(), Level.FINE)
+                .onErrorMap(throwable -> new InvalidInputException("Temporary placeholder :)"));
     }
 
     @Override
     public Mono<JobPostDto> getJobPost(Integer jobPostId) {
-        return null;
+        return webClient.get().uri(jobServiceUrl + "/" + jobPostId)
+                .retrieve()
+                .bodyToMono(JobPostDto.class)
+                .log(log.getName(), Level.FINE)
+                .onErrorMap(throwable -> new InvalidInputException("Temporary placeholder :)"));
     }
 
     @Override
     public Flux<JobPostDto> getJobPosts(int page, int size) {
-        return null;
+        return webClient.get().uri(jobServiceUrl, uriBuilder -> uriBuilder
+                        .queryParam("page", page)
+                        .queryParam("size", size)
+                        .build())
+                .retrieve()
+                .bodyToFlux(JobPostDto.class)
+                .log(log.getName(), Level.FINE)
+                .onErrorMap(throwable -> new InvalidInputException("Temporary placeholder :)"));
     }
 
     @Override
     public Mono<ResumeDto> getResume(Integer resumeId) {
-        return null;
+        return webClient.get().uri(resumeServiceUrl + "/" + resumeId)
+                .retrieve()
+                .bodyToMono(ResumeDto.class)
+                .log(log.getName(), Level.FINE)
+                .onErrorMap(throwable -> new InvalidInputException("Temporary placeholder :)"));
     }
 
     @Override
     public Flux<ResumeDto> getResumes(int page, int size) {
-        return null;
+        return webClient.get().uri(resumeServiceUrl, uriBuilder -> uriBuilder
+                        .queryParam("page", page)
+                        .queryParam("size", size)
+                        .build())
+                .retrieve()
+                .bodyToFlux(ResumeDto.class)
+                .log(log.getName(), Level.FINE)
+                .onErrorMap(throwable -> new InvalidInputException("Temporary placeholder :)"));
     }
 
 }
