42e1bbd13f1d598761d3f79107829ad0b883b868
Rakhmonov <get_info@bk.ru>
2022-05-05 11:02:34 +0500

    Update CompositeApi and CompositeController

--- backend/jobs/api/src/main/java/ru/rsh12/api/composite/api/CompositeApi.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/composite/api/CompositeApi.java
@@ -2,12 +2,44 @@ package ru.rsh12.api.composite.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import ru.rsh12.api.composite.dto.CompanyJobPosts;
 import ru.rsh12.api.core.company.dto.CompanyDto;
+import ru.rsh12.api.core.job.dto.JobPostDto;
+import ru.rsh12.api.core.resume.dto.ResumeDto;
 
 public interface CompositeApi {
 
-    @GetMapping("/api/v1/composite/companies/{companyId}")
+    @GetMapping(value = "/api/v1/composite/companies/{companyId}", produces = "application/json")
     Mono<CompanyDto> getCompany(@PathVariable("companyId") Integer companyId);
+
+    @GetMapping(value = "/api/v1/composite/companies", produces = "application/json")
+    Flux<CompanyDto> getCompanies(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size);
+
+    @GetMapping(value = "/api/v1/composite/companies/{companyId}/jobs")
+    Mono<CompanyJobPosts> getCompanyJobPosts(
+            @PathVariable("companyId") Integer companyId,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size);
+
+    @GetMapping(value = "/api/v1/composite/jobs/{jobPostId}", produces = "application/json")
+    Mono<JobPostDto> getJobPost(@PathVariable("jobPostId") Integer jobPostId);
+
+    @GetMapping(value = "/api/v1/composite/jobs", produces = "application/json")
+    Flux<JobPostDto> getJobPosts(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size);
+
+    @GetMapping(value = "/api/v1/composite/resumes/{resumeId}", produces = "application/json")
+    Mono<ResumeDto> getResume(@PathVariable("resumeId") Integer resumeId);
+
+    @GetMapping(value = "/api/v1/composite/resumes", produces = "application/json")
+    Flux<ResumeDto> getResumes(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size);
 
 }
--- backend/jobs/microservices/composite-service/src/main/java/ru/rsh12/composite/controller/CompositeController.java
+++ backend/jobs/microservices/composite-service/src/main/java/ru/rsh12/composite/controller/CompositeController.java
@@ -4,9 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.rsh12.api.composite.api.CompositeApi;
+import ru.rsh12.api.composite.dto.CompanyJobPosts;
 import ru.rsh12.api.core.company.dto.CompanyDto;
+import ru.rsh12.api.core.job.dto.JobPostDto;
+import ru.rsh12.api.core.resume.dto.ResumeDto;
+
+import java.util.List;
+import java.util.logging.Level;
 
 @RestController
 public class CompositeController implements CompositeApi {
@@ -24,6 +31,52 @@ public class CompositeController implements CompositeApi {
     public Mono<CompanyDto> getCompany(Integer companyId) {
         return integration.getCompany(companyId)
                 .doOnError(ex -> log.warn("get company by id failed: {}", ex.toString()));
+    }
+
+    @Override
+    public Flux<CompanyDto> getCompanies(int page, int size) {
+        return integration.getCompanies(page, size)
+                .doOnError(ex -> log.warn("get companies failed: {}", ex.toString()));
+    }
+
+    @Override
+    public Mono<CompanyJobPosts> getCompanyJobPosts(Integer companyId, int page, int size) {
+        return Mono.zip(values -> createCompanyAggregate((CompanyDto) values[0], (List<JobPostDto>) values[1]),
+                        integration.getCompany(companyId),
+                        integration.getCompanyJobPosts(companyId, page, size).collectList())
+                .doOnError(ex -> log.warn("get company's job posts failed: {}", ex.toString()))
+                .log(log.getName(), Level.FINE);
+    }
+
+    private CompanyJobPosts createCompanyAggregate(CompanyDto companyDto, List<JobPostDto> jobPostDtos) {
+        String companyServiceAddress = companyDto.serviceAddress();
+        String jobPostServiceAddress = jobPostDtos.stream().findFirst().map(JobPostDto::serviceAddress).orElse("");
+
+        return new CompanyJobPosts(companyDto, jobPostDtos, companyServiceAddress, jobPostServiceAddress);
+    }
+
+    @Override
+    public Mono<JobPostDto> getJobPost(Integer jobPostId) {
+        return integration.getJobPost(jobPostId)
+                .doOnError(ex -> log.warn("get job post by id failed: {}", ex.toString()));
+    }
+
+    @Override
+    public Flux<JobPostDto> getJobPosts(int page, int size) {
+        return integration.getJobPosts(page, size)
+                .doOnError(ex -> log.warn("get job posts failed: {}", ex.toString()));
+    }
+
+    @Override
+    public Mono<ResumeDto> getResume(Integer resumeId) {
+        return integration.getResume(resumeId)
+                .doOnError(ex -> log.warn("get resume by id failed: {}", ex.toString()));
+    }
+
+    @Override
+    public Flux<ResumeDto> getResumes(int page, int size) {
+        return integration.getResumes(page, size)
+                .doOnError(ex -> log.warn("get resumes failed: {}", ex.toString()));
     }
 
 }
