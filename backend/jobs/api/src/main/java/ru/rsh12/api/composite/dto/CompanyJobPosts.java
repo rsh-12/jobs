3d23682f484cef9933206737778235edaec08545
Rakhmonov <get_info@bk.ru>
2022-05-05 11:00:39 +0500

    Replace Set with List

--- backend/jobs/api/src/main/java/ru/rsh12/api/composite/dto/CompanyJobPosts.java
+++ backend/jobs/api/src/main/java/ru/rsh12/api/composite/dto/CompanyJobPosts.java
@@ -3,11 +3,11 @@ package ru.rsh12.api.composite.dto;
 import ru.rsh12.api.core.company.dto.CompanyDto;
 import ru.rsh12.api.core.job.dto.JobPostDto;
 
-import java.util.Set;
+import java.util.List;
 
 public record CompanyJobPosts(
         CompanyDto company,
-        Set<JobPostDto> jobPosts,
+        List<JobPostDto> jobPosts,
         String companyServiceAddress,
         String jobServiceAddress) {
 
