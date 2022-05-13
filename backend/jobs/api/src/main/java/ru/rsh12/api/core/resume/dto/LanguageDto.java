311f3da866d7901ed8e1fdad6cf20cbded48bbd3
Rakhmonov <get_info@bk.ru>
2022-05-13 13:50:02 +0500

    Rename LanguageDto to ResumeLanguageDto

--- backend/jobs/api/src/main/java/ru/rsh12/api/core/resume/dto/LanguageDto.java
+++
@@ -1,4 +0,0 @@
-package ru.rsh12.api.core.resume.dto;
-
-public record LanguageDto(Integer id, String name, String level) {
-}
---
+++ backend/jobs/api/src/main/java/ru/rsh12/api/core/resume/dto/ResumeLanguageDto.java
@@ -0,0 +1,4 @@
+package ru.rsh12.api.core.resume.dto;
+
+public record ResumeLanguageDto(Integer id, String name, String level) {
+}
