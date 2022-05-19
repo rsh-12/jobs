dde2d365f23aeaf59e0ed9e0d6f582c5cd145628
Rakhmonov <get_info@bk.ru>
2022-05-19 23:07:41 +0500

    Add eureka-server module

---
+++ backend/jobs/spring-cloud/eureka-server/src/test/java/ru/rsh12/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsh12.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
