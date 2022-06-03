7686590e238135740dd223524be52eb9a6c63760
Rakhmonov <get_info@bk.ru>
2022-06-03 17:52:24 +0500

    Add config-server

---
+++ backend/jobs/spring-cloud/config-server/src/test/java/ru/rsh12/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsh12.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
